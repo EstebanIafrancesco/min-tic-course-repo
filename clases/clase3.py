e07ed4309fa5c6cd788b118a7080c75b2a61a4c1
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-05-05 00:40:36 -0500

    Some basic knowledge about printing and varibles in python

---
+++ clases/clase3.py
@@ -0,0 +1,46 @@
+print('Clase 3 - de 04/05/2021')
+print('--------------------------------------------------------')
+
+# Declaramos tres variables tipo entero A,B y C
+
+A = 10 
+B = 15
+C = 20
+
+print('Valor inicial de A:', A)
+print('Valor inicial de B:', B)
+print('Valor inicial de C:', C)
+
+A = A + B
+B = B + 8
+C = C + A
+A = A + 5
+B = B + 3
+C = C + 2
+A = A - B
+B = A - B
+C = A - B
+
+# Valores finales
+print ('Valores finales: \n')
+print('Valor final de A:', A)
+print('Valor final de B:', B)
+print('Valor final de C:', C)
+
+
+# concatenando cadenas
+
+saludo = 'hola como estas '
+nombre = 'Esteban'
+saludoPersonal = saludo + nombre
+print(saludoPersonal)
+
+# condicionales
+
+a = 1
+b = 4
+if a < b
+    print(a,' es mas pequeño que ', b)
+# else
+#     print(b,' es mas pequeño que ', a)
+
