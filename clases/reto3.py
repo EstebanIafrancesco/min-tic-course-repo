a2b68d5331b3a370ee8e6e7014e840ebda8805ba
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-06-01 00:16:06 -0500

    Parámetros iniciales para el reto 3

---
+++ clases/reto3.py
@@ -0,0 +1,27 @@
+# By Esteban Iafrancesco
+# 01-06-2021
+# Reto 2 curso de programacion Mision TIC
+
+# Ee requiere que escriba una función qué reciba cómo parámetro:
+# una lista de palabras y que con la ayuda de ciclos la salida muestre o devuelva las palabras
+# abreviadas dentro de una lista así:
+
+# Consideremos una palabra demasiado larga, si su longitud es estrictamente superior a 10
+# caracteres. Todas las palabras demasiado largas deben reemplazarse por una abreviatura
+# especial.
+# Esta abreviatura se hace así: escribimos la primera y la última letra de una palabra y entre
+# ellas escribimos el número de letras entre la primera y la última letra. Ese número está en
+# sistema decimal y no contiene ceros a la izquierda.
+# Por lo tanto, "Esternocleidomastoideo" se escribirá como "e20o" y "otorrinolaringólogo” se
+# escribirá como "o17o".
+
+
+lista = ['oro','localizacion','internationalizacion','electroencefalograma']
+
+def palabras(lista)->list:
+    return 'Estoy dentro de la funcion pues mijo...'
+
+
+print()
+print(palabras(lista))
+print()
