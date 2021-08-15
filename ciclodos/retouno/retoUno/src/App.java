c3dbc2cefa45e1a0ed7b38ae0b5425ea06b698fd
Esteban Iafrancesco <eiafrancescoa@dane.gov.co>
2021-08-14 22:31:25 -0500

    Solución reto 1 ciclo2

--- ciclodos/retouno/retoUno/src/App.java
+++ ciclodos/retouno/retoUno/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("\nHola amiguitos de MIntic!\n");
+
+        NuevoProyecto np = new NuevoProyecto(12, 10000000, 0.9);
+        System.out.println("\n");
+        System.out.println(np.calcularInteresSimple());
+        System.out.println(np.calcularInteresCompuesto());
+        System.out.println(np.compararInversion());
+        
+        System.out.println("\n");
     }
 }
--- ciclodos/retouno/retoUno/src/NuevoProyecto.java
+++ ciclodos/retouno/retoUno/src/NuevoProyecto.java
@@ -1,48 +1,50 @@
 public class NuevoProyecto {
     // -----------------------------------------------------------------
     // Atributos
-    double pMonto;
-    double pInteres;
-    int pPeriodo;
-    double diferencia;
-    double interesSimple;
-    double interesCompuesto;
     // -----------------------------------------------------------------
+    private double monto;
+    private double interes;
+    private int periodo;
     // -----------------------------------------------------------------
     // Métodos
     // -----------------------------------------------------------------
-    public NuevoProyecto( int pPeriodo, double pMonto, double pInteres){
-        pMonto = this.pMonto;
-        pInteres = this.pInteres;
-        pPeriodo = this.pPeriodo;
 
+    // Constructores
+    public NuevoProyecto(){
+
+        this.monto = 0.0;
+        this.interes = 0.0;
+        this.periodo = 0;
     }
+    public NuevoProyecto(int pPeriodo, double pMonto, double pInteres){
+        this.monto = pMonto;
+        this.interes = pInteres;
+        this.periodo = pPeriodo;
+
+    } 
+
     public double calcularInteresSimple(){
-        return  Math.round(pMonto*pInteres+pPeriodo);
+        double interesSimple = this.monto*this.interes*this.periodo/100;
+        return  Math.round(interesSimple);
     }
-    // calacularInteresCompuesto(){
-    //     interesCompuesto = Math.round(pMonto*((Math.pow((1+pInteres), pPeriodo))-1));
-    // }
-    
-    /**
-    * Método para comparar la diferencia en el total de intereses generados para
-    el proyecto.
-    * @return Respuesta al Reto.
-    */
-    public String compararInversion ( int pPeriodo, double pMonto, double  pInteres ){
-        
-        return "hola";
-        
-    }
-    // public double compararInversion (){
-    //     //codigo
-    // }
-        public static void main(String[] args){
-        //Instanciación de clase y uso de métodos
-            NuevoProyecto np = new NuevoProyecto(5, 12.5 ,33.6 );
-            System.out.println(np.calcularInteresSimple());
-
-        }
+    public double calcularInteresCompuesto(){
+        double interesCompuesto = this.monto*((Math.pow((1+(this.interes/100)), this.periodo))-1);
+        return  Math.round(interesCompuesto);
     }
 
-    
+    public double compararInversion ( int pPeriodo, double pMonto, double  pInteres){
+
+        this.periodo = pPeriodo;
+        this.monto = pMonto;
+        this.interes = pInteres;
+
+        double diferencia = calcularInteresCompuesto() - calcularInteresSimple();
+        return diferencia;
+        
+    }
+    public double compararInversion(){
+        double diferencia = calcularInteresCompuesto() - calcularInteresSimple();
+        return diferencia;
+    }
+
+}
